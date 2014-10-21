a5f44370fdab486cbbd475586e36795a679f0d41
Nicolas Franck <nicolas.franck@ugent.be>
2014-10-21 10:52:34 +0200

    milla setup

---
+++ cpanfile
@@ -0,0 +1,4 @@
+requires 'perl','5.10.1';
+requires 'Catmandu','0.05';
+requires 'Dancer','0';
+recommends 'Catmandu::Store::DBI','0.042';
--- lib/Dancer/Plugin/Auth/RBAC/Credentials/Catmandu.pm
+++ lib/Dancer/Plugin/Auth/RBAC/Credentials/Catmandu.pm
@@ -1,74 +1,87 @@
 package Dancer::Plugin::Auth::RBAC::Credentials::Catmandu;
+our $VERSION = "0.01";
 use Catmandu::Sane;
 use Catmandu::Util qw(require_package :is);
 use Catmandu qw(store);
 use base qw/Dancer::Plugin::Auth::RBAC::Credentials/;
 
 sub bag {
-  my($self,$args)=@_;
-  state $bag = store($args->{store})->bag($args->{bag});
+    my($self,$args)=@_;
+    state $bag = store($args->{store})->bag($args->{bag});
 }
 
 sub authorize {
-  my($self,$options,@arguments) = @_;
-  my($login,$password) = @arguments;
+    my($self,$options,@arguments) = @_;
+    my($login,$password) = @arguments;
 
-  #what are you doing here? You're already in!
-  my $user = $self->credentials;
-  if(is_hash_ref($user) && ($user->{id} || $user->{login}) && !@{$user->{error}}){
-    return $user;
-  }
+    #what are you doing here? You're already in!
+    my $user = $self->credentials;
+    if(is_hash_ref($user) && ($user->{id} || $user->{login}) && !@{$user->{error}}){
 
-  if(!(is_string($login) && is_string($password))){
+        return $user;
 
-    $self->errors('login and password are required');
-    return;
+    }
 
-  }
-  
-  # authorize a new account using supplied credentials
-  my $account = $self->bag($options)->get($login);
+    if(!(is_string($login) && is_string($password))){
 
-  if(!is_hash_ref($account)){
+        $self->errors('login and password are required');
+        return;
 
-    $self->errors('login and/or password is invalid');
-    return;  
-  
-  }
+    }
 
-  if(!is_string($account->{password})){
+    # authorize a new account using supplied credentials
+    my $account = $self->bag($options)->get($login);
 
-    $self->errors('attempting to access as inaccessible account');
-    return;
+    if(!is_hash_ref($account)){
 
-  }
+        $self->errors('login and/or password is invalid');
+        return;
 
-  if($account->{password} ne $password){
+    }
 
-    $self->errors('login and/or password is invalid');
-    return;
-    
-  }
+    if(!is_string($account->{password})){
 
-  my $session_data = {
-    id    => $account->{_id},
-    name  => $account->{name} || ucfirst($login),
-    login => $account->{login},
-    roles => [@{$account->{roles}}],
-    error => []
-  };
+        $self->errors('attempting to access as inaccessible account');
+        return;
 
-  return $self->credentials($session_data);
+    }
+
+    if($account->{password} ne $password){
+
+        $self->errors('login and/or password is invalid');
+        return;
+
+    }
+
+    my $session_data = {
+        id    => $account->{_id},
+        name  => $account->{name} || ucfirst($login),
+        login => $account->{login},
+        roles => [@{$account->{roles}}],
+        error => []
+    };
+
+    return $self->credentials($session_data);
 
 }
 
 =head1 NAME
 
-  Dancer::Plugin::Auth::RBAC::Credentials::Catmandu - Catmandu store backend for Dancer::Plugin::RBAC::Credentials
+Dancer::Plugin::Auth::RBAC::Credentials::Catmandu - Catmandu store backend for Dancer::Plugin::RBAC::Credentials
 
-=head1 CONFIGURATION
+=head1 INSTALLATION AND CONFIGURATION
 
-=head3 catmandu.yml
+=head2 install the following perl modules
+
+=over
+
+=item Catmandu
+
+=item Catmandu::DBI
+
+=back
+
+=head2 add the yaml file 'catmandu.yml' to the root directory of your Dancer project
 
 store:
  default:
@@ -78,15 +91,52 @@ store:
    username: "admin"
    password: "admin"
 
-=head3 config.yml  
+=head2 adjust your Dancer config.yml
 
 plugins:
  Auth::RBAC:
   credentials:
    class: Catmandu
    options:
-    store: default
-    bag: users
+    #name of store in catmandu.yml
+    store: 'default'
+    #name of table
+    bag: 'users'
+
+=head2 The table 'users' will be created if not exists already, and will have the following format
+
+=over
+
+=item id
+
+identifier of the user
+
+=item data
+
+json data, in the following form:
+
+{
+
+    _id: "njfranck",
+    login: "njfranck",
+    name: "Nicolas Franck",
+    password: "password",
+    roles: ["admin"]
+
+}
+
+=back
+
+=head2 in order to add users execute the following code
+
+Catmandu->store('default')->bag('users')->add({
+    _id => "user2",
+    name => "user 2",
+    login => "user2",
+    password => "secret",
+    roles => ["editor","messenger"]
+
+});
 
 =head1 AUTHOR
 
@@ -95,6 +145,9 @@ Nicolas Franck, C<< <nicolas.franck at ugent.be> >>
 =head1 SEE ALSO
 
 L<Catmandu>
+
+L<Catmandu::Store::DBI>
+
 L<Dancer::Plugin::Auth::RBAC>
 
 =head1 LICENSE AND COPYRIGHT
